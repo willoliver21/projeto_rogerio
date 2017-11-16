5973d376f22d407a5a830716ff3b89e149b75b44
willoliver21 <will.oliver.ti@outlook.com>
2017-11-16 18:07:13 -0200

    Complementando Add Clientes
    Novas Querys(A Testar!)

--- add-clientes.php
+++ add-clientes.php
@@ -17,12 +17,26 @@ $tel = $_POST["tel"];
 $cel = $_POST["cel"];
 $data = $_POST["data"];
 $renda = $_POST["renda"];
+$rua = $_POST["rua"];
+$numero = $_POST["numero"];
+$bairro = $_POST["bairro"];
+$complemento = $_POST["complemento"];
+$cep = $_POST["cep"];
 
 $conexao->begin_transaction(MYSQLI_TRANS_START_READ_ONLY);
 
-$conexao->query("INSERT INTO pessoa('nome', 'rg_ie', 'cpf_cnpj, 'data_cadastro', 'data_fim', 'telefone', 'celular', 'endereco_id_endereco', 'Login_id_login')
-                VALUES ('$nome', '$rgie', '$cpfcnpj', 'Data Cadastro', 'null', '$tel', '$cel', 'BUSCA ID ENDEREÇO', 'ID Login')");
+$conexao->query("INSERT INTO endereco('rua', 'numero', 'bairro', 'complemento', 'cep', 'estado_id_estado')
+                  VALUES ('$rua', '$numero', '$bairro', '$complemento', '$cep', '1')");
 
+$id = mysql_insert_id();
+
+$conexao->query("INSERT INTO pessoa('nome', 'rg_ie', 'cpf_cnpj, 'data_cadastro', 'data_fim', 'telefone', 'celular', 'endereco_id_endereco', 'Login_id_login')
+                VALUES ('$nome', '$rgie', '$cpfcnpj', 'null', 'null', '$tel', '$cel', '$id', 'null')");
+
+$id = mysql_insert_id();
+
+$conexao->query("INSERT INTO cliente('data_nascimento', 'renda', '$id')
+                 VALUES ('null', '$data', ''$renda', '$id')");
 // Adicionar outras Querys
 
 $conexao->commit();
