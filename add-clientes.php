fde44f2b2e5d0accef285221647b77ef7070ae36
willoliver21 <33660533+willoliver21@users.noreply.github.com>
2017-11-20 21:28:00 -0300

    Adicionado Função insereHistorico
    Adicionado Pagina controle-historico com funções de historico e alterado no add-clientes a mesma.

--- add-clientes.php
+++ add-clientes.php
@@ -1,5 +1,6 @@
 <?php
 include("conecta.php");
+include("controle_historico.php");
 
 $nome = $_POST["nome"];
 $mail = $_POST["mail"];
@@ -19,8 +20,6 @@ $cep = $_POST["cep"];
 $query = "INSERT INTO `endereco`(`rua`, `numero`, `bairro`, `complemento`, `cep`, `estado_id_estado`)
 VALUES ('$rua', '$numero', '$bairro', '$complemento', '$cep', '1')";
 
-
-
 mysqli_query($conexao, $query);
 
 $id = mysqli_insert_id($conexao);
@@ -35,7 +34,8 @@ $id = mysqli_insert_id($conexao);
 $query = "INSERT INTO `cliente`(`data_nascimento`, `renda`, `pessoa_id_pessoa`)
 VALUES ('$data', '$renda', '$id')";
 
-
+//CHAMAR FUNÇÃO INSEREHISTORICO EM TODAS OPERAÇÕES
+insereHistorico($conexao, 1, 1, 20-11-2017, "Descricao");
 
 mysqli_query($conexao, $query);
 
