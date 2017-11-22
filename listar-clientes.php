724e20ff50eebf29717c863bb5ef417f32d35176
icaperin <32550744+icaperin@users.noreply.github.com>
2017-11-22 13:42:46 -0300

    Update listar-clientes.php

--- listar-clientes.php
+++ listar-clientes.php
@@ -8,8 +8,6 @@ include("cabecalho.php");
    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
    <link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.10.5/css/jquery.dataTables.css">
    <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.10.5/jquery.dataTables.js"></script>
-
-
 </head>
 <body>
 
@@ -35,7 +33,7 @@ include("cabecalho.php");
       $('#example').dataTable({
         //"sDom": '<"top"i>rt<"bottom"flp><"clear">',
         "bProcessing": true,
-        "sAjaxSource": "pro.php",
+        "sAjaxSource": "busca-clientes-lista.php",
         "aoColumns": [
               { mData: 'id_cliente' },
               { mData: 'data_nascimento' },
