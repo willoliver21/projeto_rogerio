715bd799a70b9a16d711e2e1311ef3c0a2d19bfc
icaperin <32550744+icaperin@users.noreply.github.com>
2017-11-22 14:14:36 -0300

    Update busca-clientes-lista.php

--- busca-clientes-lista.php
+++ busca-clientes-lista.php
@@ -2,7 +2,11 @@
   include("conecta.php");
 //require('conecta.php');
 
-$sql = "select * from cliente";
+$sql = "select p.nome, p.rg_ie, p.cpf_cnpj, p.data_cadastro, p.telefone, c.renda, c.data_nascimento, e.rua, e.numero, e.bairro, e.cep  from cliente c
+join pessoa p on p.id_pessoa = c.pessoa_id_pessoa
+join endereco e on e.id_endereco = p.endereco_id_endereco
+where c.data_nascimento is not null";
+
 $result = $conexao->query($sql);
 
 while($row = $result->fetch_array(MYSQLI_ASSOC)){
