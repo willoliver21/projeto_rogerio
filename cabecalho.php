1fe444a24421516888da95a34d0df4e3b94972ea
icaperin <32550744+icaperin@users.noreply.github.com>
2017-11-22 19:49:29 -0300

    Add files via upload

--- cabecalho.php
+++ cabecalho.php
@@ -1,40 +1,51 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt">
 <head>
-	<title>TADS4</title>
+  <title>Menu</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!--//estrutura da pagina
-	//as tags nao sao fechadas aqui.....sao fechadas no rodape
-	//tag 'class' vem do boot strap -->
-	<meta charset="utf-8">
-	<link href="css/bootstrap.css" rel="stylesheet">
-	<link href="css/estilo.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div class="navbar navbar-inverse navbar-fixed-top">
-		<div class="container">
-			<div class="navbar-header">
-				<a href="index.php" class="navbar-brand">TADS4</a>
-			</div>
-			<div>
-				<ul class="nav navbar-nav">
-                    <li><a href="produto-formulario.php">Produtos</a></li>
-                    <!-- NÃO PRECISA DISSO
-										<li><a href="produto-lista.php">Listar Produtos</a></li>
-										!-->
-										<li><a href="cadastro-cliente-formulario.php">Cliente</a></li>
-										<li><a href="cadastro-funcionario-formulario.php">Funcionario</a></li>
-										<li><a href="cadastro-estado-formulario.php">Estado</a></li>
-										<li><a href="cadastro-cargo-formulario.php">Cargo</a></li>
-										<li><a href="cadastro-fornecedor-formulario.php">Fornecedor</a></li>
-										<li><a href="cadastro-ramoatividade-formulario.php">Ramo Atividade</a></li>
-										<li><a href="cadastro-unidademedida-formulario.php">Unidade de Medida</a></li>
-										<li><a href="cadastro-categoria-formulario.php">Categoria</a></li>
-										<li><a href="listar-clientes.php">Listar Clientes</a></li>
-                </ul>
-			</div>
-		</div>
-	</div>
 
-	<div class="container">
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">STOCK</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.php">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Cadastro <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="cadastro-cliente-formulario.php">Novo Cliente</a></li>
+          <li><a href="cadastro-fornecedor-formulario.php">Novo Fornecedor</a></li>
+          <li><a href="cadastro-funcionario-formulario.php">Novo Funcionário</a></li>
+		  		<li><a href="produto-formulario.php">Novo Produto</a></li>
+		  		<li><a href="cadastro-cargo-formulario.php">Novo Cargo</a></li>
+					<li><a href="cadastro-ramoatividade-formulario.php">Novo Ramo Atividade</a></li>
+					<li><a href="cadastro-unidademedida-formulario.php">Nova Unidade Medida</a></li>
+					<li><a href="cadastro-cargo-formulario.php">Nova Categoria</a></li>
 
-	<div class="principal">
+        </ul>
+		<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Consultar <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="listar-clientes.php">Clientes</a></li>
+          <li><a href="#">Fornecedores</a></li>
+          <li><a href="#">Funcionários</a></li>
+		  	  <li><a href="#">Produtos</a></li>
+
+        </ul>
+      </li>
+      <li><a href="#">Ajuda</a></li>
+
+    </ul>
+  </div>
+</nav>
+
+
+<div class="container">
+
+<div class="principal">
